e2131f6208506f9ffd32e4b558ed9746f2825777
Avik Nandy <anandy@wdg.co>
2018-12-06 16:46:00 -0500

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Applications/MAMP/htdocs/gabrlknght.github.io/node_modules/gatsby-plugin-emotion/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Applications/MAMP/htdocs/gabrlknght.github.io/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    }]
--- .cache/sync-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-
-exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Applications/MAMP/htdocs/gabrlknght.github.io/node_modules/gatsby-plugin-offline/app-shell.js")),
-  "component---src-pages-index-jsx": preferDefault(require("/Applications/MAMP/htdocs/gabrlknght.github.io/src/pages/index.jsx"))
-}
-
