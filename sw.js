70c05980ae1f3417447ae335e47b46a293b18c8c
Avik Nandy <anandy@wdg.co>
2018-12-11 10:41:45 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-962a8347bbcde79d2916.js"
+    "url": "webpack-runtime-c71fcdfefdea66fdc721.js"
   },
   {
     "url": "app-ee9dab75cc95da1b903b.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "72a23b1c713aaab9288b5139520d2804"
+    "revision": "b8e54d42c44c9be5ac3a170704515848"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
